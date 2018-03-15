c3c12611f6b6c6f75cbcb1123a849b68bfc253da
Andrey Zaklitskiy <glockman94@gmail.com>
2018-03-15 21:03:59 +0200

    sliders-slick

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,4 @@
+//home-page-main-slider
 var slideIndex = 1;
 showSlides(slideIndex);
 function plusSlides(n) {
@@ -25,3 +26,44 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className+="active";
 }
+
+
+//slider
+$(document).ready(function(){
+  $('.home-featured-slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
+
