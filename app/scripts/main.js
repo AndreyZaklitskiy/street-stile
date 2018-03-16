71e40b19fab82067eb02acc290eca9b3a3d423cd
Andrey Zaklitskiy <glockman94@gmail.com>
2018-03-16 15:21:40 +0200

    sliders

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,41 +1,21 @@
 //home-page-main-slider
-var slideIndex = 1;
-showSlides(slideIndex);
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("home-slider-slide");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {//если n > длинны массива slides
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex=slides.length
-  }
-  for (i=0; i < slides.length; i++) {
-    slides[i].style.display="none";
-  }
-  for(i=0; i < dots.length; i++) {
-    dots[i].className=dots[i].className.replace("active","");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className+="active";
-}
+$(document).ready(function() {
+  $('.home-slider').slick({
+    dots: false,
+    arrows: true
+  });
+});
 
 
 //slider
 $(document).ready(function(){
   $('.home-featured-slider').slick({
-    dots: true,
+    dots: false,
+    arrows: true,
     infinite: false,
     speed: 300,
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 1024,
@@ -60,10 +40,8 @@ $(document).ready(function(){
           slidesToScroll: 1
         }
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ]
   });
 });
+
 
