1a90e86354e80f97d3cba863364292100b899769
Andrey Zaklitskiy <glockman94@gmail.com>
2018-03-18 17:28:54 +0200

    newsletter

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -44,4 +44,38 @@ $(document).ready(function(){
   });
 });
 
-
+//sponsors-slider
+$('.brand-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
